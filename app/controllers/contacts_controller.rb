1cc937f1b2594057d7ccff20d8f9daff1dabd38a
Caio <caiohenriqueads@gmail.com>
2023-02-27 20:26:47 -0300

    apply bootstrap style

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,7 +5,11 @@ class ContactsController < ApplicationController
 
   # GET /contacts or /contacts.json
   def index
-    @contacts = Contact.search(params[:full_name], fields: [:full_name])
+    @contacts = if params[:full_name].present?
+                  Contact.search(params[:full_name], fields: [:full_name])
+                else
+                  Contact.all
+                end
   end
 
   # GET /contacts/1 or /contacts/1.json
